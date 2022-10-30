e7c257ca59bc31c3253c4371bb4bbd9e4134ba34
eliasjp <illumine1998@gmail.com>
2022-10-30 13:38:26 -0300

    feat/ postDOM & footer for post index

--- src/scripts/localStorage.js
+++ src/scripts/localStorage.js
@@ -1,3 +1,9 @@
 export function storageNews (object){
     window.localStorage.setItem("news-storage", JSON.stringify(object))
 }
+
+export function checkNews (){
+    const post = window.localStorage.getItem("news-storage")
+    const parsePost = JSON.parse(post)
+    return parsePost
+}
